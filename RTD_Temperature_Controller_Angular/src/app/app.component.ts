164069d8282c58675f4c8925903bb7fd706a69a1
Subin A M <subindavid605@gmail.com>
2023-11-09 14:43:32 +0530

    Changed API name

--- RTD_Temperature_Controller_Angular/src/app/app.component.ts
+++ RTD_Temperature_Controller_Angular/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'RTD_Temperature_Controller_Angularhdf';
+  title = 'RTD_Temperature_Controller_Angular';
 }
