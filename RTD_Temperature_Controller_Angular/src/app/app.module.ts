027736c28b98b91c00f117eac661dcf047025c4c
Subin A M <subindavid605@gmail.com>
2023-11-09 16:46:44 +0530

    added nav bar

--- RTD_Temperature_Controller_Angular/src/app/app.module.ts
+++ RTD_Temperature_Controller_Angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BannerComponent } from './banner/banner.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BannerComponent
+    BannerComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
