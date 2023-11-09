7525f11df745c1d8777c42186c6950dac69ab297
alby0203 <aj333@cet.ac.in>
2023-11-09 15:13:24 +0530

    Created connection component

--- RTD_Temperature_Controller_Angular/src/app/app.module.ts
+++ RTD_Temperature_Controller_Angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConnectionComponent } from './connection/connection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnectionComponent
   ],
   imports: [
     BrowserModule,
