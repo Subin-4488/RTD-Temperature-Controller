9ab739c11c29e75b3adbe93bfaa08d500f0aaa30
alby0203 <aj333@cet.ac.in>
2023-11-10 14:36:24 +0530

    SettingUI

--- RTD_Temperature_Controller_Angular/src/app/settings/settings.component.ts
+++ RTD_Temperature_Controller_Angular/src/app/settings/settings.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
+
 export class SettingsComponent {
+  
+  settingsForm:FormGroup
+
+  constructor(fb:FormBuilder){
+    this.settingsForm=fb.group({
+      threshold:[],
+      dar:[],
+      current4:[],
+      current20:[],
+      c1:[],
+      c2:[],
+      c3:[]
+    })
+  }
+  onSubmit(value:string){
+    console.log(value)
+  }
 
 }
