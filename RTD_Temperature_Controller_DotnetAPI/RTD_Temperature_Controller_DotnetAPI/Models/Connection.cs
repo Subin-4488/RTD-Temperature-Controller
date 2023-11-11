6ae696b9110c6e2d0d35b4396ffb83ed6b536765
Subin A M <subindavid605@gmail.com>
2023-11-11 13:58:30 +0530

    updated models in backend

--- RTD_Temperature_Controller_DotnetAPI/RTD_Temperature_Controller_DotnetAPI/Models/Connection.cs
+++ RTD_Temperature_Controller_DotnetAPI/RTD_Temperature_Controller_DotnetAPI/Models/Connection.cs
@@ -1,6 +1,44 @@
 ﻿namespace RTD_Temperature_Controller_DotnetAPI.Models
 {
+    public enum Bps
+    {
+        Bps_1 = 1200,
+        Bps_2 = 2400,
+        Bpe_3 = 4800,
+        Bps_4 = 9600,
+        Bps_5 = 14400,
+        Bps_6 = 38400 
+    }
+
+    public enum DataBits
+    {
+        DataBit_1 = 5,
+        DataBit_2 = 6,
+        DataBit_3 = 7,
+        DataBit_4 = 8,
+    }
+
+    public enum Parity
+    {
+        None,
+        Even,
+        Odd,
+        Mark,
+        Space
+    }
+
+    public struct StopBits
+    {
+        public const int StopBits_1 = 1;
+        public const double StopBits_2 = 1.5; 
+        public const int StopBits_3 = 2;
+    }
+
     public class Connection
     {
+        public Bps BitsPerSecond { get; set; }
+        public DataBits DataBits { get; set; }
+        public Parity Parity { get; set; }
+        public StopBits StopBits { get; set; }
     }
 }
--- RTD_Temperature_Controller_DotnetAPI/RTD_Temperature_Controller_DotnetAPI/Models/Settings.cs
+++ RTD_Temperature_Controller_DotnetAPI/RTD_Temperature_Controller_DotnetAPI/Models/Settings.cs
@@ -1,6 +1,20 @@
 ﻿namespace RTD_Temperature_Controller_DotnetAPI.Models
 {
+    public enum Colors
+    {
+        red, green, blue 
+    }
+
     public class Settings
     {
+        public double Threshold { get; set; }
+        public int DataAcquisitionRate { get; set; }
+
+        public double Temperature_4mA { get; set; }
+        public double Temperature_20mA { get; set; }
+
+        public Colors Color_0_15 { get; set; }
+        public Colors Color_16_30 { get; set; }
+        public Colors Color_31_45 { get; set; }
     }
 }
