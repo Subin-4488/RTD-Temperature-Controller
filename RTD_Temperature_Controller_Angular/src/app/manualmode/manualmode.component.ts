8820e979ac69669057e57c2169c980013e8d0f27
alby0203 <aj333@cet.ac.in>
2023-11-13 16:29:50 +0530

    Manual mode led working

--- RTD_Temperature_Controller_Angular/src/app/manualmode/manualmode.component.ts
+++ RTD_Temperature_Controller_Angular/src/app/manualmode/manualmode.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ManualmodeService } from '../services/manualmode.service';
+import { Command } from '../models/Command';
 
 @Component({
   selector: 'app-manualmode',
@@ -10,8 +12,9 @@ export class ManualmodeComponent {
 
   pwmFormGroup: FormGroup;
   submitted: boolean = false
+  newCommand:Command = new Command('','')
 
-  constructor(fb: FormBuilder){
+  constructor(fb: FormBuilder,private manualmodeService:ManualmodeService){
     this.pwmFormGroup = fb.group({
       pwmCycleControl: ['', [Validators.required, Validators.pattern("^0*(?:[0-9][0-9]?|100)$")]]
     })
@@ -28,4 +31,55 @@ export class ManualmodeComponent {
 
     console.log("submitted: "+this.pwmFormGroup.value.pwmCycleControl);
   }
+
+  setLed1(){
+    const ele = document.getElementById('led1') as HTMLInputElement
+    console.log(ele.checked)
+    if(ele.checked){
+      this.newCommand.Value="SET LED ON 1\r"
+    }
+    else{
+      console.log("gdfsg")
+      this.newCommand.Value="SET LED OFF 1\r"
+    }
+    this.newCommand.Command="Set"
+    //this.newCommand.Value="SET LED ON 1\r"
+    this.manualmodeService.sendCommand(this.newCommand).subscribe(data=>{
+      console.log(data)
+    })
+  }
+  setLed2(){
+    const ele = document.getElementById('led2') as HTMLInputElement
+    console.log(ele.checked)
+    if(ele.checked){
+      this.newCommand.Value="SET LED ON 2\r"
+    }
+    else{
+      console.log("gdfsg")
+      this.newCommand.Value="SET LED OFF 2\r"
+    }
+    this.newCommand.Command="Set"
+    //this.newCommand.Value="SET LED ON 2\r"
+    this.manualmodeService.sendCommand(this.newCommand).subscribe(data=>{
+      console.log(data)
+    })
+  }
+  setLed3(){
+    const ele = document.getElementById('led3') as HTMLInputElement
+    console.log(ele.checked)
+    if(ele.checked){
+      this.newCommand.Value="SET LED ON 3\r"
+    }
+    else{
+      console.log("gdfsg")
+      this.newCommand.Value="SET LED OFF 3\r"
+    }
+    this.newCommand.Command="Set"
+    //this.newCommand.Value="SET LED ON 3\r"
+    this.manualmodeService.sendCommand(this.newCommand).subscribe(data=>{
+      console.log(data)
+    })
+  }
+
+
 }
