5fb95f4dbdee3dc3979adeb81eeabed03997a1d6
alby0203 <aj333@cet.ac.in>
2023-11-20 11:54:04 +0530

    Chart set as default

--- RTD_Temperature_Controller_Angular/src/app/home/home.component.ts
+++ RTD_Temperature_Controller_Angular/src/app/home/home.component.ts
@@ -55,8 +55,8 @@ export class HomeComponent implements OnDestroy {
     axisX: {
       type: Date,
       title: "Time (seconds)",
-       valueFormatString: "HH:mm:ss",
-      //  interval: 1,  //data acquisition rate
+      valueFormatString: "HH:mm:ss",
+      // interval: 1,  //data acquisition rate
       // intervalType: "second",
      
 
@@ -68,8 +68,8 @@ export class HomeComponent implements OnDestroy {
       type: "line",
       lineThickness: 2,
       dataPoints: this.dataPoints,
-      markerColor: "red",
-      lineColor:this.current_selection,
+      markerColor: "black",
+      //lineColor:this.current_selection,
     }]
   }
 
@@ -82,53 +82,54 @@ export class HomeComponent implements OnDestroy {
       this.settings.color_0_15 = Colors[Number(data.color_0_15)]
       this.settings.color_16_30 = Colors[Number(data.color_16_30)]
       this.settings.color_31_45 = Colors[Number(data.color_31_45)]
-      this.updateData();
+      //this.updateData();
     })     
   }
   
   ngOnDestroy() {
     clearTimeout(this.timeout);
+    this.hubService.hubConnection.off('UpdateTemperature')
   }
  
-  updateData = () => { 
-    let temperatureData: number[] = [];
-    this.hubService.temperatureFromSensor.forEach(d => {
-      temperatureData.push(d)
-    })  
-    this.addData(temperatureData)
-      this.hubService.temperatureFromSensor = []
-  }
+  // updateData = () => { 
+  //   let temperatureData: number[] = [];
+  //   this.hubService.temperatureFromSensor.forEach(d => {
+  //     temperatureData.push(d)
+  //   })  
+  //   this.addData(temperatureData)
+  //     this.hubService.temperatureFromSensor = []
+  // }
   
-  addData = (data:any[]) => {
+  // addData = (data:any[]) => {
 
-    if(data.length > 1) {
-      data.forEach( (val:any) => {
+  //   if(data.length > 1) {
+  //     data.forEach( (val:any) => {
         
-        this.dataPoints.push({x: new Date(val.time), y: parseInt(val.temperature),  lineColor: this.getColor(parseInt(val.temperature))});
+  //       this.dataPoints.push({x: new Date(val.time), y: parseInt(val.temperature),  lineColor: this.getColor(parseInt(val.temperature))});
         
-        var formattedTime = this.datePipe.transform(val.time, 'HH:mm:ss')
-        //  console.log("Recur: "+formattedTime+":"+val.temperature+":"+this.getColor(parseInt(data[0].temperature)))
+  //       var formattedTime = this.datePipe.transform(val.time, 'HH:mm:ss')
+  //       //  console.log("Recur: "+formattedTime+":"+val.temperature+":"+this.getColor(parseInt(data[0].temperature)))
         
-      })
-      data = []
+  //     })
+  //     data = []
 
-    } 
-    else {
-      var formattedTime = this.datePipe.transform(data[0].time, 'HH:mm:ss')
-      // console.log("normal: "+formattedTime+":"+data[0].temperature)
+  //   } 
+  //   else {
+  //     var formattedTime = this.datePipe.transform(data[0].time, 'HH:mm:ss')
+  //     // console.log("normal: "+formattedTime+":"+data[0].temperature)
         
-      this.dataPoints.push({x: new Date(data[0].time), y: parseInt(data[0].temperature), lineColor: this.getColor(parseInt(data[0].temperature))});
-      this.current_selection = this.getColor(parseInt(data[0].temperature))
+  //     this.dataPoints.push({x: new Date(data[0].time), y: parseInt(data[0].temperature), lineColor: this.getColor(parseInt(data[0].temperature))});
+  //     this.current_selection = this.getColor(parseInt(data[0].temperature))
       
-    }
-    this.chart.render();
-    if (this.dataPoints.length>4){
-      while (this.dataPoints.length>=4)
-        this.dataPoints.shift();
-    }
-    this.timeout = setTimeout(this.updateData, this.settings.dataAcquisitionRate*1000);  //data acquisition rate
+  //   }
+  //   this.chart.render();
+  //   if (this.dataPoints.length>4){
+  //     while (this.dataPoints.length>=4)
+  //       this.dataPoints.shift();
+  //   }
+  //   this.timeout = setTimeout(this.updateData, this.settings.dataAcquisitionRate*1000);  //data acquisition rate
 
-  }
+  // }
 
   getColor(temperature: number): string {
     if (temperature >= 0 && temperature <= 15) {
@@ -147,13 +148,36 @@ export class HomeComponent implements OnDestroy {
   }
 
   graphInitializer(){
-    if (!this.sensor_status){
-      this.sensor_status=true;
-      
-      this.home_service.sendCommand(new Command("GET","GET TMP")).subscribe(d =>{
 
+    this.sensor_status = !this.sensor_status
+    if(this.sensor_status){
+      //console.log("hello")
+      this.home_service.sendCommand(new Command("GET","GET TMP\r")).subscribe(d=>{
+        if(d==true){
+          this.hubService.hubConnection.on('UpdateTemperature',(temperatureData) =>{
+            console.log(this.getColor(parseInt(temperatureData.temperature)))
+            console.log(this.dataPoints)
+            this.dataPoints.push({x: new Date(temperatureData.time), y: parseInt(temperatureData.temperature),  lineColor: this.getColor(parseInt(temperatureData.temperature))});
+            //this.dataPoints.push({x: new Date(temperatureData.time), y: parseInt(temperatureData.temperature)});
+
+            if(this.dataPoints.length>20)
+              this.dataPoints.shift()
+            this.chart.render()
+          })
+        }
       })
-
     }
+    else{
+      this.hubService.hubConnection.off('UpdateTemperature')
+    }
+
+    // if (!this.sensor_status){
+    //   this.sensor_status=true;
+      
+    //   this.home_service.sendCommand(new Command("GET","GET TMP")).subscribe(d =>{
+
+    //   })
+
+    // }
   }
 }                               
--- RTD_Temperature_Controller_DotnetAPI/RTD_Temperature_Controller_DotnetAPI/Controllers/ConnectionController.cs
+++ RTD_Temperature_Controller_DotnetAPI/RTD_Temperature_Controller_DotnetAPI/Controllers/ConnectionController.cs
@@ -98,9 +98,11 @@ namespace RTD_Temperature_Controller_DotnetAPI.Controllers
             try
             {
                 _serialPort.Open();
+                byte[] bytes = Encoding.UTF8.GetBytes("GET VER\r");
+                //_serialPort.Write(bytes, 0, bytes.Length);
                 _status = true;
-                //_thread = new Thread(sendRandom);
-                //_thread.Start();
+                _thread = new Thread(sendRandom);
+                _thread.Start();
                 return true;
             }
             catch(Exception ex) {
