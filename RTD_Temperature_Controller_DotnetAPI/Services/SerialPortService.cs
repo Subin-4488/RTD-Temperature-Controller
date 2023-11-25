a36c3eefa352218ecdd759bf0421567850a8cff6
alby0203 <aj333@cet.ac.in>
2023-11-25 17:03:20 +0530

    Multiple tabs

--- RTD_Temperature_Controller_DotnetAPI/Services/SerialPortService.cs
+++ RTD_Temperature_Controller_DotnetAPI/Services/SerialPortService.cs
@@ -5,11 +5,18 @@ namespace Services
 {
     public class SerialPortService : ISerialPortService
     {
-        private readonly SerialPort _serialPort;
+        private  SerialPort _serialPort;
         public SerialPort SerialPort { get => _serialPort;  }
 
         public SerialPortService()
         {
+            _serialPort = new SerialPort();
+        }
+
+        public void deleteMethod()
+        {
+            if (_serialPort != null && _serialPort.IsOpen)
+                _serialPort.Close();
             _serialPort = new SerialPort();
         }
     }
