1ad29e05e24f1543f52fa141d555f8aff6c37afe
alby0203 <aj333@cet.ac.in>
2023-11-27 18:04:56 +0530

    Added validation to settings component

--- RTD_Temperature_Controller_Angular/src/app/app.module.ts
+++ RTD_Temperature_Controller_Angular/src/app/app.module.ts
@@ -1,23 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BannerComponent } from './banner/banner.component';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ConnectionComponent } from './connection/connection.component';
-
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { SettingsComponent } from './settings/settings.component';
 import { ManualmodeComponent } from './manualmode/manualmode.component';
-
 import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
-import { DatePipe } from '@angular/common';
-
 import {MatButtonModule} from '@angular/material/button';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
@@ -42,7 +36,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatButtonModule
   ],
   providers: [
-    DatePipe
   ],
   bootstrap: [AppComponent]
 })
