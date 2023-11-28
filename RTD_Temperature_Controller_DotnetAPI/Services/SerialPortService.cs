c2b8b5df04f888ffd1dbb7722edf72b9652e9b5a
alby0203 <aj333@cet.ac.in>
2023-11-28 18:30:24 +0530

    Refactoring backend

--- RTD_Temperature_Controller_DotnetAPI/Services/SerialPortService.cs
+++ RTD_Temperature_Controller_DotnetAPI/Services/SerialPortService.cs
@@ -3,17 +3,26 @@ using System.IO.Ports;
 
 namespace Services
 {
+    /// <summary>
+    /// Service for managing a serial port connection
+    /// It involves reseting, opening new serial port
+    /// </summary>
     public class SerialPortService : ISerialPortService
     {
         private  SerialPort _serialPort;
         public SerialPort SerialPort { get => _serialPort;  }
 
+        /// <summary>
+        /// Creates a new serial port instance
+        /// </summary>
         public SerialPortService()
         {
             _serialPort = new SerialPort();
         }
-
-        public void deleteMethod()
+        /// <summary>
+        ///  Resets the SerialPort instance by closing the existing one (if open) and creating a new instance.
+        /// </summary>
+        public void ResetPort()
         {
             if (_serialPort != null && _serialPort.IsOpen)
                 _serialPort.Close();
