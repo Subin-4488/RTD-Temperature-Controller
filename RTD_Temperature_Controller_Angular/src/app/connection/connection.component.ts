0f3fc6b454e4a9274d8f686e5e16b639e7b1fa2f
alby0203 <aj333@cet.ac.in>
2023-11-29 18:28:25 +0530

    Added tooltip

--- RTD_Temperature_Controller_Angular/src/app/connection/connection.component.ts
+++ RTD_Temperature_Controller_Angular/src/app/connection/connection.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ConnectionService } from '../services/connection.service';
 import { Connection } from '../models/Connection';
@@ -25,7 +25,7 @@ export class ConnectionComponent {
   ) {
     localStorage.setItem("connected","false")
     this.ConnectionForm = fb.group({
-      portname: [],
+      portname: [,Validators.required],
       bps: [9600],
       databits: [8],
       parity: ['None'],
